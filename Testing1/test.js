d433cae826597fb2a8a500488a646c3dd38fea9b
Үүрцайх Норовсамбуу <23LP8969@aiRo.local>
2023-12-18 17:09:25 -0800

    Dec19

--- Testing1/test.js
+++ Testing1/test.js
@@ -1,6 +1,22 @@
-let power = Math.pow(10, 1);
-let x = Math.floor(Math.random() * 100);
+let randomNumb = (document.getElementById("rando").innerText = Math.floor(
+  Math.random() * 100 + 1
+));
 
-function newNum() {}
-newNum(x);
-console.log(x);
+function search() {
+  let inpNum = Number(document.getElementById("num").value);
+  if (inpNum > randomNumb) {
+    document.getElementById("result").innerText = "Number is greater";
+  } else if (inpNum < randomNumb) {
+    document.getElementById("result").innerText = "Number is lesser";
+  } else {
+    document.getElementById("result").innerText = "Congrats";
+  }
+}
+function newNum() {
+  randomNumb =
+    randomNumb =
+    document.getElementById("rando").innerText =
+      Math.floor(Math.random() * 100 + 1);
+  document.getElementById("result").innerText = "";
+  document.getElementById("num").value = "";
+}
